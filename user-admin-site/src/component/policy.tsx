dbc6e05edbd1bff7fa75f2729b624d141104eefd
Mark N Broadhead <MarkNBroadhead@users.noreply.github.com>
2020-10-08 16:45:08 -0600

    Add basic components for admin portal (#3)

---
+++ user-admin-site/src/component/policy.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {
+  Create,
+  Datagrid,
+  DateField,
+  Edit,
+  List,
+  ReferenceField,
+  ReferenceInput,
+  SimpleForm,
+  SelectInput,
+  TextField,
+  TextInput,
+} from "react-admin";
+
+export const PolicyList = (props: any) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="name" />
+      <ReferenceField source="serviceId" reference="service">
+        <TextField source="name" />
+      </ReferenceField>
+      <TextField source="actions" />
+      <TextField source="tables" />
+      <TextField source="hasuraServiceRole" />
+      <DateField source="createdAt" />
+      <DateField source="updatedAt" />
+    </Datagrid>
+  </List>
+);
+
+export const PolicyEdit = (props: any) => (
+  <Edit {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+      <ReferenceInput source="serviceId" reference="service">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="actions" />
+      <TextInput source="tables" />
+      <TextInput source="hasuraServiceRole" />
+      <DateField source="createdAt" />
+      <DateField source="updatedAt" />
+      <TextField source="id" />
+    </SimpleForm>
+  </Edit>
+);
+
+export const PolicyCreate = (props: any) => (
+  <Create {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+      <ReferenceInput source="serviceId" reference="service">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="actions" />
+      <TextInput source="tables" />
+      <TextInput source="hasuraServiceRole" />
+      <DateField source="createdAt" />
+      <DateField source="updatedAt" />
+      <TextField source="id" />
+    </SimpleForm>
+  </Create>
+);
