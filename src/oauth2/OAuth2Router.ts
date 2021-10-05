b67523a51000c496d0eaee3381e6ec7a04ef4f0c
raul.ascencio <raul@ravn.co>
2021-10-05 09:35:17 -0600

    ref: adding method to figure out user for session
    Signed-off-by: raul.ascencio <raul@ravn.co>

--- src/oauth2/OAuth2Router.ts
+++ src/oauth2/OAuth2Router.ts
@@ -1,4 +1,5 @@
-import { Router, Request, Response } from "express";
+import { json, urlencoded } from "body-parser";
+import express, { Router } from "express";
 
 import {
   handleExpressResponse,
@@ -7,7 +8,11 @@ import {
   requestFromExpress,
 } from "@jmondi/oauth2-server/dist/adapters/express";
 
-import { AuthorizationServer, DateInterval } from "@jmondi/oauth2-server";
+import {
+  AuthorizationServer,
+  DateInterval,
+  JwtService,
+} from "@jmondi/oauth2-server";
 import UserAuthServiceOAuth2ClientRepository from "./repositories/OAuth2ClientRepository";
 import DynamoObjectDBStore from "@tesseractcollective/serverless-toolbox/dist/objectStore/DynamoDbObjectStore";
 import Client from "./entities/Client";
@@ -17,6 +22,8 @@ import TokenRepository from "./repositories/TokenRepository";
 import Token from "./entities/Token";
 import ScopeRepository from "./repositories/ScopeRepository";
 import Scope from "./entities/Scope";
+import { UserRepository } from "./repositories/UserRepository";
+import User from "./entities/User";
 
 export default class OAuth2Router {
   readonly router = Router();
@@ -44,6 +51,11 @@ export default class OAuth2Router {
       "east-2"
     );
 
+    const userStore = new DynamoObjectDBStore<User>(
+      "oauth2_user_repository",
+      "east-2"
+    );
+
     const clientRepository = new UserAuthServiceOAuth2ClientRepository(
       clientStore
     );
@@ -55,6 +67,8 @@ export default class OAuth2Router {
     const accessTokenRepository = new TokenRepository(tokenStore);
 
     const scopeRepository = new ScopeRepository(scopeStore);
+
+    const userRepository = new UserRepository(userStore);
 
     this.authorizationServer = new AuthorizationServer(
       authCodeRepository,
@@ -71,21 +85,84 @@ export default class OAuth2Router {
     );
   }
 
+  getIsAuthorizationApprovedFromSession() {
+    return true; // TODO: implement function.
+  }
+
   setupRoutes() {
-    this.router.post("/token", async (req: Request, res: Response) => {
-      const request = requestFromExpress(req);
-      const response = responseFromExpress(res);
-      try {
-        const oauthResponse =
-          await this.authorizationServer.respondToAccessTokenRequest(
-            request,
-            response
-          );
-        return handleExpressResponse(res, oauthResponse);
-      } catch (e) {
-        handleExpressError(e, res);
-        return false;
+    this.router.use(json());
+    this.router.use(urlencoded({ extended: false }));
+
+    this.router.post(
+      "/token",
+      async (req: express.Request, res: express.Response) => {
+        const request = requestFromExpress(req);
+        const response = responseFromExpress(res);
+        try {
+          const oauthResponse =
+            await this.authorizationServer.respondToAccessTokenRequest(
+              request,
+              response
+            );
+          return handleExpressResponse(res, oauthResponse);
+        } catch (e) {
+          handleExpressError(e, res);
+          return;
+        }
       }
-    });
+    );
+
+    this.router.get(
+      "/authorize",
+      async (req: express.Request, res: express.Response) => {
+        const request = requestFromExpress(req);
+
+        try {
+          // Validate the HTTP request and return an AuthorizationRequest.
+          const authRequest =
+            await this.authorizationServer.validateAuthorizationRequest(
+              request
+            );
+
+          // You will probably redirect the user to a login endpoint.
+          if (!req.user) {
+            res.redirect("/login");
+            return;
+          }
+          // After login, the user should be redirected back with user in the session.
+          // You will need to manage the authorization query on the round trip.
+          // The auth request object can be serialized and saved into a user's session.
+
+          // Once the user has logged in set the user on the AuthorizationRequest
+          authRequest.user = req.user;
+
+          // Once the user has approved or denied the client update the status
+          // (true = approved, false = denied)
+          authRequest.isAuthorizationApproved =
+            this.getIsAuthorizationApprovedFromSession();
+
+          // If the user has not approved the client's authorization request,
+          // the user should be redirected to the approval screen.
+          if (!authRequest.isAuthorizationApproved) {
+            // This form will ask the user to approve the client and the scopes requested.
+            // "Do you authorize Jason to: read contacts? write contacts?"
+            res.redirect("/scopes");
+            return;
+          }
+
+          // At this point the user has approved the client for authorization.
+          // Any last authorization requests such as Two Factor Authentication (2FA) can happen here.
+
+          // Redirect back to redirect_uri with `code` and `state` as url query params.
+          const oauthResponse =
+            await this.authorizationServer.completeAuthorizationRequest(
+              authRequest
+            );
+          return handleExpressResponse(res, oauthResponse);
+        } catch (e) {
+          handleExpressError(e, res);
+        }
+      }
+    );
   }
 }
